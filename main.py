e1ff7292b92150f9ecca093e7a0eb890b9a1a65b
nnnoopt <nnnoopt@yandex.ru>
2022-11-16 20:53:36 +0300

    add new chek and change field size

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ def greet():
 
 def show():
     print()
-    print("    | 1 | 2 | 3 | ")
+    print(f"    | 1 | 2 | 3 | ")
     print("  --------------- ")
+
     for i, row in enumerate(field):
         row_str = f"  {i + 1} | {' | '.join(row)} | "
         print(row_str)
@@ -22,13 +23,17 @@ def show():
 
 
 def chek_win(z):
-
     for i, r in enumerate(field):
         if r == [z for _ in range(n)]:
             return True
-        for j, f, v in zip(field[0], field[1], field[2]):
-            if j == z and f == z and v == z:
-                return True
+
+    arr = numpy.array(field)
+    arr_transpose = arr.transpose()
+    a = arr_transpose.tolist()
+
+    for i, r in enumerate(a):
+        if r == [z for _ in range(n)]:
+            return True
 
     cnt1 = 0
     cnt2 = 0
@@ -45,24 +50,55 @@ def chek_win(z):
     return False
 
 
+def enter_coord2(num):
+
+    if num == 1:
+        y = input("введи координату X: ")
+        x = input("введи координату Y: ")
+
+        if y.isdigit() and y.isdigit():
+
+            y = int(y)
+            x = int(x)
+            y -= 1
+            x -= 1
+            if all([0 <= x < n, 0 <= y < n]):
+
+                if field[x][y] != "O" and field[x][y] != "X":
+                    field[x][y] = "X"
+                else:
+                    print("поле уже занято.")
+                    enter_coord2(1)
+
+            else:
+                print("неверные данные, введите еще раз.")
+                print(x, y)
+                enter_coord2(1)
+
+        else:
+            print("введено не число.")
+            enter_coord2(1)
+
+    if num == 2:
+        x = random.randint(0, 2)
+        y = random.randint(0, 2)
+        if all([0 <= x <= 3, 0 <= y <= 3, field[x][y] != "O", field[x][y] != "X"]):
+            field[x][y] = "O"
+        else:
+            enter_coord2(2)
+
+
 def enter_coord():
     global x
     global y
 
-    y = int(input("введи координату X: "))-1
-    x = int(input("введи координату Y: "))-1
-
-    # if y.isdigit() and y.isdigit():
-    #     x = int(x)
-    #     y = int(y)
-    # else:
-    #     print("введены не цифры")
-    #     enter_coord()
+    y = int(input("введи координату X: ")) - 1
+    x = int(input("введи координату Y: ")) - 1
 
     if all([0 <= x <= n, 0 <= y <= n, field[x][y] != "O", field[x][y] != "X"]):
         field[x][y] = "X"
     else:
-        print("введи верные координаты")
+        print("введи верные координаты.")
         enter_coord()
 
 
@@ -78,8 +114,10 @@ def ai_enter_coord():
 
 
 greet()
-n = 3
+
+n = int(input("введи размер поля: "))
 field = [[" "] * n for i in range(n)]
+print(field)
 show()
 
 x = 0
@@ -87,7 +125,8 @@ y = 0
 
 while True:
 
-    enter_coord()
+    # enter_coord()
+    enter_coord2(1)
     show()
     if chek_win("X"):
         print("Победил человек. \nКонец игры.")
@@ -99,7 +138,8 @@ while True:
         break
 
     print("ходит компьютер.")
-    ai_enter_coord()
+    enter_coord2(2)
+    # ai_enter_coord()
 
     show()
 
